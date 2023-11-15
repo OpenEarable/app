ec0c6238107af254c3239b92bc11562f0784b4a5
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2023-11-15 14:46:32 +0100

    removed unused apps

--- open_earable/lib/apps_tab.dart
+++ open_earable/lib/apps_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:open_earable/apps/posture_tracker/model/earable_attitude_tracker.dart';
 import 'package:open_earable/apps/posture_tracker/model/mock_attitude_tracker.dart';
 import 'package:open_earable/apps/posture_tracker/view/posture_tracker_view.dart';
+import 'package:open_earable/apps/recorder.dart';
 import 'package:open_earable_flutter/src/open_earable_flutter.dart';
 
 import './apps/step_counter.dart';
@@ -10,7 +11,7 @@ class AppInfo {
   final IconData iconData;
   final String title;
   final String description;
-  final void Function(BuildContext, OpenEarable) onTap;
+  final VoidCallback onTap;
 
   AppInfo(
     {
@@ -22,62 +23,55 @@ class AppInfo {
   );
 }
 
-List<AppInfo> sampleApps = [
-  AppInfo(
-      iconData: Icons.directions_walk,
-      title: "Step Counter",
-      description: "Counts number of steps taken.",
-      onTap: (_, openEarable) {
-        // Action when the card is tapped, for example:
-        // Navigator.push(context, MaterialPageRoute(builder: (context) => PostureTracker()));
-      }),
-  AppInfo(
-      iconData: Icons.face_6,
-      title: "Posture Tracker",
-      description: "Get feedback on bad posture.",
-      onTap: (context, OpenEarable openEarable) {
-        // Action when the card is tapped, for example:
-        Navigator.push(context, MaterialPageRoute(builder: (context) => PostureTrackerView(EarableAttitudeTracker(openEarable))));
-      }),
-  AppInfo(
-      iconData: Icons.lunch_dining,
-      title: "Asissted Dietary Monitoring",
-      description: "Detect eating episodes.",
-      onTap: (_, openEarable) {
-        // Action when the card is tapped, for example:
-        // Navigator.push(context, MaterialPageRoute(builder: (context) => PostureTracker()));
-      }),
-  AppInfo(
-      iconData: Icons.height,
-      title: "Jump Height Test",
-      description: "Test your maximum jump height.",
-      onTap: (_, openEarable) {
-        // Action when the card is tapped, for example:
-        // Navigator.push(context, MaterialPageRoute(builder: (context) => PostureTracker()));
-      }),
-  // ... similarly for other apps
-];
-
 class AppsTab extends StatelessWidget {
   final OpenEarable _openEarable;
 
   AppsTab(this._openEarable);
 
+  List<AppInfo> sampleApps(BuildContext context) {
+    return [
+      AppInfo(
+      iconData: Icons.face_6,
+      title: "Posture Tracker",
+      description: "Get feedback on bad posture.",
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PostureTrackerView(EarableAttitudeTracker(_openEarable))
+          )
+        );
+      }),
+      AppInfo(
+          iconData: Icons.fiber_smart_record,
+          title: "Recorder",
+          description: "Counts number of steps taken.",
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Recorder(_openEarable)));
+          }),
+      // ... similarly for other apps
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<AppInfo> apps = sampleApps(context);
     return ListView.builder(
-      itemCount: sampleApps.length,
+      itemCount: apps.length,
       itemBuilder: (BuildContext context, int index) {
         return Card(
           color: Theme.of(context).colorScheme.primary,
-          margin: EdgeInsets.all(8.0),
+          margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
           child: ListTile(
-            leading: Icon(sampleApps[index].iconData, size: 40.0),
-            title: Text(sampleApps[index].title),
-            subtitle: Text(sampleApps[index].description),
+            leading: Icon(apps[index].iconData, size: 40.0),
+            title: Text(apps[index].title),
+            subtitle: Text(apps[index].description),
             trailing: Icon(Icons.arrow_forward_ios,
                 size: 16.0), // Arrow icon on the right
-            onTap: () { sampleApps[index].onTap(context, _openEarable); }, // Callback when the card is tapped
+            onTap: apps[index].onTap, // Callback when the card is tapped
           ),
         );
       },
