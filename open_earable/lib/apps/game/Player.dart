cee53ced6cb896798c0d597a11ed616bafeb48c6
Elena <elivas2000@googlemail.com>
2024-01-07 14:11:56 +0100

    Add game MazeRace

---
+++ open_earable/lib/apps/game/Player.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Player extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('lib/apps/game/images/mouse.png');
+  }
+}
