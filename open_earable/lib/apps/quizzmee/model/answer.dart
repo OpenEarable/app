5c799ac2c5a540e2998340776aed675610160cdc
D0M1W <nini.wlcek@mail.com>
2024-01-08 20:59:33 +0100

    Added changes to branch

---
+++ open_earable/lib/apps/quizzmee/model/answer.dart
@@ -0,0 +1,7 @@
+class Answer {
+  final String text;
+  final bool isCorrect;
+
+  Answer({required this.text, required this.isCorrect});
+
+}
---
+++ open_earable/lib/apps/quizzmee/model/quiz.dart
@@ -0,0 +1,8 @@
+import 'package:open_earable/apps/quizzmee/model/question.dart';
+
+class Quiz {
+  final List<Question> questions;
+
+  Quiz({required this.questions});
+
+}
