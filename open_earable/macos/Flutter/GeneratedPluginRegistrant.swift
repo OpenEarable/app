a6c60d554cc8e24641f0d19ad2e3524fc240fad3
Niklas Kerkhoff <niklas.kerkhoff@icloud.com>
2024-01-09 01:18:55 +0100

    added dependencies

--- open_earable/macos/Flutter/GeneratedPluginRegistrant.swift
+++ open_earable/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,12 @@ import Foundation
 
 import flutter_gl_macos
 import path_provider_foundation
+import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterGlMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterGlMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
--- open_earable/windows/flutter/generated_plugin_registrant.cc
+++ open_earable/windows/flutter/generated_plugin_registrant.cc
@@ -8,10 +8,13 @@
 
 #include <flutter_gl_windows/flutter_gl_windows_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterGlWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterGlWindowsPlugin"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
