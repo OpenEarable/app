e73eefe7505b9ecb59f1eede5654ac466dcb0b84
Oliver Bagge <bagge@teco.edu>
2024-04-11 20:30:59 +0200

    add new 3d model and use new 3d graphics library model_viewer_plus

--- open_earable/lib/sensor_data_tab/earable_3d_model.dart
+++ open_earable/lib/sensor_data_tab/earable_3d_model.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:open_earable/shared/earable_not_connected_warning.dart';
-import 'package:provider/provider.dart';
 import 'dart:async';
-import 'package:three_dart/three_dart.dart' as three;
-import 'package:three_dart_jsm/three_dart_jsm.dart' as three_jsm;
-import 'package:flutter_gl/flutter_gl.dart';
-import 'package:three_dart/three3d/math/euler.dart';
-import 'package:open_earable_flutter/src/open_earable_flutter.dart';
 import 'dart:math';
-import 'package:open_earable/ble/ble_controller.dart';
+import 'package:open_earable_flutter/src/open_earable_flutter.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Earable3DModel extends StatefulWidget {
   final OpenEarable _openEarable;
@@ -18,6 +13,7 @@ class Earable3DModel extends StatefulWidget {
 }
 
 class _Earable3DModelState extends State<Earable3DModel> {
+  WebViewController? _controller;
   final OpenEarable _openEarable;
   _Earable3DModelState(this._openEarable);
   StreamSubscription? _imuSubscription;
@@ -26,44 +22,11 @@ class _Earable3DModelState extends State<Earable3DModel> {
   double _roll = 0;
 
   final String fileName = "assets/OpenEarable.obj";
-  Euler _euler = Euler(0, 0, 0, "YZX");
-
-  late FlutterGlPlugin three3dRender;
-  three.WebGLRenderer? renderer;
-
-  int? fboId;
-  bool kIsWeb = false;
-  late double width;
-  late double height;
-
-  Size? screenSize;
-
-  late three.Scene scene;
-  three.Color _sceneBackground = three.Color.fromArray([0, 0, 0]);
-  bool _sceneInitialized = false;
-  late three.Camera camera;
-  late three.Mesh mesh;
-
-  double dpr = 1.0;
-
-  var amount = 4;
-
-  bool verbose = true;
-  bool disposed = false;
-
-  late three.Object3D object;
-
-  late three.Texture texture;
-
-  late three.WebGLRenderTarget renderTarget;
 
   dynamic sourceTexture;
   @override
   void initState() {
     super.initState();
-    //_earableModel = EarableModel(fileName: "assets/OpenEarable.obj");
-    three3dRender = FlutterGlPlugin();
-
     if (_openEarable.bleManager.connected) {
       _setupListeners();
     }
@@ -79,211 +42,40 @@ class _Earable3DModelState extends State<Earable3DModel> {
   _setupListeners() {
     _imuSubscription =
         _openEarable.sensorManager.subscribeToSensorData(0).listen((data) {
-      // Yaw (around Z-axis)
-      _yaw = data["EULER"]["YAW"];
-      // Pitch (around Y-axis)
-      _pitch = data["EULER"]["PITCH"];
-      // Roll (around X-axis)
-      _roll = data["EULER"]["ROLL"];
-      updateRotation(roll: _roll, pitch: _pitch, yaw: _yaw);
+      setState(() {
+        _yaw = data["EULER"]["YAW"];
+        _pitch = data["EULER"]["PITCH"];
+        _roll = data["EULER"]["ROLL"];
+      });
+      _controller?.runJavaScript(
+          "document.querySelector('model-viewer').setAttribute('orientation', '${-_pitch} ${_roll} ${-_yaw}');");
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!Provider.of<BluetoothController>(context).connected) {
-      return EarableNotConnectedWarning();
-    } else {
-      return Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            // child: Text(title, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-          ),
-          Expanded(child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              width = constraints.maxWidth;
-              height = constraints.maxHeight;
-              Color c = Theme.of(context).colorScheme.background;
-              _sceneBackground =
-                  three.Color.fromArray([c.red, c.green, c.blue]);
-              initSize(context);
-              return Column(
-                children: [
-                  Stack(
-                    children: [
-                      Container(
-                          width: width,
-                          height: height,
-                          color: Theme.of(context).colorScheme.background,
-                          child: Builder(builder: (BuildContext context) {
-                            if (kIsWeb) {
-                              return three3dRender.isInitialized
-                                  ? HtmlElementView(
-                                      viewType:
-                                          three3dRender.textureId!.toString())
-                                  : Container();
-                            } else {
-                              return three3dRender.isInitialized
-                                  ? Texture(textureId: three3dRender.textureId!)
-                                  : Container();
-                            }
-                          })),
-                    ],
-                  ),
-                ],
-              );
-            },
-          )),
-          Padding(
-              padding: EdgeInsets.only(bottom: 16),
-              child: Text(
-                  "Yaw: ${(_yaw * 180 / pi).toStringAsFixed(1)}°\nPitch: ${(_pitch * 180 / pi).toStringAsFixed(1)}°\nRoll: ${(_roll * 180 / pi).toStringAsFixed(1)}°"))
-        ],
-      );
-    }
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    Map<String, dynamic> options = {
-      "antialias": true,
-      "alpha": false,
-      "width": width.toInt(),
-      "height": height.toInt(),
-      "dpr": dpr
-    };
-
-    await three3dRender.initialize(options: options);
-    // Wait for web
-    Future.delayed(const Duration(milliseconds: 100), () async {
-      await three3dRender.prepareContext();
-
-      initScene();
-    });
-  }
-
-  initSize(BuildContext context) {
-    if (screenSize != null) {
-      return;
-    }
-
-    final mqd = MediaQuery.of(context);
-
-    screenSize = mqd.size;
-    dpr = mqd.devicePixelRatio;
-
-    initPlatformState();
-  }
-
-  void updateRotation({
-    required double yaw,
-    required double pitch,
-    required double roll,
-  }) {
-    setState(() {});
-    // Signs and order of RPY angles need to be swapped to display correctly
-    _euler.x = roll;
-    _euler.y = -yaw;
-    _euler.z = -pitch;
-    if (_sceneInitialized && three3dRender.isInitialized) {
-      render();
-    }
-  }
-
-  render() {
-    object.setRotationFromEuler(_euler);
-
-    int t = DateTime.now().millisecondsSinceEpoch;
-
-    final gl = three3dRender.gl;
-    scene.add(three.AxesHelper(75));
-    scene.castShadow = false;
-    scene.receiveShadow = false;
-    renderer!.alpha = true;
-    renderer!.render(scene, camera);
-
-    int t1 = DateTime.now().millisecondsSinceEpoch;
-
-    if (verbose) {
-      print("render cost: ${t1 - t} ");
-      print(renderer!.info.memory);
-      print(renderer!.info.render);
-    }
-
-    // 重要 更新纹理之前一定要调用 确保gl程序执行完毕
-    gl.flush();
-
-    if (verbose) print(" render: sourceTexture: $sourceTexture ");
-
-    if (!kIsWeb) {
-      three3dRender.updateTexture(sourceTexture);
-    }
-  }
-
-  initRenderer() {
-    Map<String, dynamic> options = {
-      "width": width,
-      "height": height,
-      "gl": three3dRender.gl,
-      "antialias": true,
-      "canvas": three3dRender.element,
-    };
-    renderer = three.WebGLRenderer(options);
-    renderer!.setPixelRatio(dpr);
-    renderer!.setSize(width, height, false);
-    renderer!.shadowMap.enabled = false;
-
-    if (!kIsWeb) {
-      var pars = three.WebGLRenderTargetOptions({"format": three.RGBAFormat});
-      renderTarget = three.WebGLRenderTarget(
-          (width * dpr).toInt(), (height * dpr).toInt(), pars);
-      renderTarget.samples = 4;
-      renderer!.setRenderTarget(renderTarget);
-      sourceTexture = renderer!.getRenderTargetGLTexture(renderTarget);
-    }
-  }
-
-  initScene() async {
-    initRenderer();
-    await initPage();
-    _sceneInitialized = true;
-  }
-
-  initPage() async {
-    camera = three.PerspectiveCamera(45, width / height, 1, 2000);
-    camera.position.z = 250;
-    camera.position.x = 50;
-    camera.position.y = 50;
-    camera.lookAt(three.Vector3(0, 0, 0));
-    _euler.order = "YZX";
-    // scene
-
-    scene = three.Scene();
-
-    var ambientLight = three.AmbientLight(0xcccccc, 0.4);
-    scene.add(ambientLight);
-
-    var pointLight = three.PointLight(0xffffff, 0.8);
-    camera.add(pointLight);
-    scene.add(camera);
-
-    var loader = three_jsm.OBJLoader(null);
-    object = await loader.loadAsync(fileName);
-
-    //object.scale.set(0.5, 0.5, 0.5);
-    scene.add(object);
-    scene.background = _sceneBackground;
-    animate();
-  }
-
-  animate() {
-    if (disposed) {
-      return;
-    }
-    _yaw += 0.05;
-    //_pitch += 0.5;
-    //_roll += 0.5;
-    render();
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Expanded(
+          child: ModelViewer(
+              cameraControls: false,
+              backgroundColor: Theme.of(context).colorScheme.background,
+              src: 'assets/OpenEarableV2-L.glb',
+              alt: 'A 3D model of an astronaut',
+              interactionPrompt: InteractionPrompt.none,
+              autoRotate: false,
+              disableZoom: true,
+              disablePan: true,
+              onWebViewCreated: (controller) {
+                _controller = controller;
+                controller.runJavaScript(
+                    "document.body.style.overflow = 'hidden';" +
+                        "document.documentElement.style.overflow = 'hidden';" +
+                        "document.addEventListener('touchmove', function(e) { e.preventDefault(); }, { passive: false });");
+              })),
+      Padding(
+          padding: EdgeInsets.only(bottom: 16),
+          child: Text(
+              "Yaw: ${(_yaw * 180 / pi).toStringAsFixed(1)}°\nPitch: ${(_pitch * 180 / pi).toStringAsFixed(1)}°\nRoll: ${(_roll * 180 / pi).toStringAsFixed(1)}°"))
+    ]);
   }
 }
--- open_earable/macos/Flutter/GeneratedPluginRegistrant.swift
+++ open_earable/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_gl_macos
 import path_provider_foundation
 import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterGlMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterGlMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
