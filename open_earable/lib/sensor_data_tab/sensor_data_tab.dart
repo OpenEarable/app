f14af649d2031918d3484b42e06b1f95369da017
Jonas Greifenhain <cadivus@daverkomp.de>
2024-10-23 02:45:45 +0200

    Generate list of charts depending on earable version

--- open_earable/lib/sensor_data_tab/sensor_data_tab.dart
+++ open_earable/lib/sensor_data_tab/sensor_data_tab.dart
@@ -44,13 +44,46 @@ class _InternalSensorDataTab extends StatefulWidget {
 class _InternalSensorDataTabState extends State<_InternalSensorDataTab>
     with TickerProviderStateMixin {
   late TabController _tabController;
-  late TabController _v1TabController;
+
+  late List<Widget> dataChartWidgets;
+  late List<Tab> dataChartTabs;
+
+  void initDataCharts() {
+    List<EarableDataChart> dataCharts = EarableDataChart.getAvailableDataCharts(
+      widget.openEarable,
+      widget.isV2,
+    );
+
+    dataChartWidgets = [
+      ...dataCharts,
+      Earable3DModel(widget.openEarable),
+    ];
+    dataChartTabs = [
+      ...dataCharts.map((e) => Tab(text: e.shortTitle)),
+      Tab(text: '3D'),
+    ];
+    _tabController = TabController(
+      vsync: this,
+      length: dataChartWidgets.length,
+    );
+  }
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(vsync: this, length: 10);
-    _v1TabController = TabController(vsync: this, length: 6);
+    initDataCharts();
+  }
+
+  @override
+  void didUpdateWidget(covariant _InternalSensorDataTab oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.openEarable == widget.openEarable &&
+        oldWidget.isV2 == widget.isV2) {
+      return;
+    }
+
+    initDataCharts();
   }
 
   @override
@@ -59,157 +92,22 @@ class _InternalSensorDataTabState extends State<_InternalSensorDataTab>
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(kToolbarHeight), // Default AppBar height
-        child: widget.isV2 ? v2TabBar() : v1TabBar(),
+        child: TabBar(
+          isScrollable: true,
+          controller: _tabController,
+          // Color of the underline indicator
+          indicatorColor: Colors.white,
+          // Color of the active tab label
+          labelColor: Colors.white,
+          // Color of the inactive tab labels
+          unselectedLabelColor: Colors.grey,
+          tabs: dataChartTabs,
+        ),
       ),
-      body: widget.isV2 ? v2TabBarView() : v1TabBarView(),
-    );
-  }
-
-  Widget v1TabBar() {
-    return TabBar(
-      isScrollable: true,
-      controller: _v1TabController,
-      // Color of the underline indicator
-      indicatorColor: Colors.white,
-      // Color of the active tab label
-      labelColor: Colors.white,
-      // Color of the inactive tab labels
-      unselectedLabelColor: Colors.grey,
-      tabs: [
-        Tab(text: 'Acc.'),
-        Tab(text: 'Gyro.'),
-        Tab(text: 'Magn.'),
-        Tab(text: 'Press.'),
-        Tab(text: 'Temp. (A.)'),
-        Tab(text: '3D'),
-      ],
-    );
-  }
-
-  Widget v2TabBar() {
-    return TabBar(
-      isScrollable: true,
-      controller: _tabController,
-      indicatorColor: Colors.white,
-      // Color of the underline indicator
-      labelColor: Colors.white,
-      // Color of the active tab label
-      unselectedLabelColor: Colors.grey,
-      // Color of the inactive tab labels
-      tabs: [
-        Tab(text: 'Acc.'),
-        Tab(text: 'Gyro.'),
-        Tab(text: 'Magn.'),
-        Tab(text: 'Press.'),
-        Tab(text: 'Temp. (A.)'),
-        Tab(text: 'Temp. (S.)'),
-        Tab(text: 'HR'),
-        Tab(text: 'SpO2'),
-        Tab(text: 'PPG'),
-        Tab(text: '3D'),
-      ],
-    );
-  }
-
-  Widget v1TabBarView() {
-    return TabBarView(
-      controller: _v1TabController,
-      children: [
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'ACC',
-          chartTitle: 'Accelerometer',
-          shortTitle: 'Acc.',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'GYRO',
-          chartTitle: 'Gyroscope',
-          shortTitle: 'Gyro.',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'MAG',
-          chartTitle: 'Magnetometer',
-          shortTitle: 'Magn.',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'BARO',
-          chartTitle: 'Pressure',
-          shortTitle: 'Press.',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'TEMP',
-          chartTitle: 'Temperature (Ambient)',
-          shortTitle: 'Temp. (A.)',
-        ),
-        Earable3DModel(widget.openEarable),
-      ],
-    );
-  }
-
-  Widget v2TabBarView() {
-    return TabBarView(
-      controller: _tabController,
-      children: [
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'ACC',
-          chartTitle: 'Accelerometer',
-          shortTitle: 'Acc.',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'GYRO',
-          chartTitle: 'Gyroscope',
-          shortTitle: 'Gyro.',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'MAG',
-          chartTitle: 'Magnetometer',
-          shortTitle: 'Magn.',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'BARO',
-          chartTitle: 'Pressure',
-          shortTitle: 'Press.',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'TEMP',
-          chartTitle: 'Temperature (Ambient)',
-          shortTitle: 'Temp. (A.)',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'OPTTEMP',
-          chartTitle: 'Temperature (Surface)',
-          shortTitle: 'Temp. (S.)',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'PULSOX',
-          chartTitle: 'Heart Rate',
-          shortTitle: 'HR',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'PULSOX',
-          chartTitle: 'SpO2',
-          shortTitle: 'SpO2',
-        ),
-        EarableDataChart(
-          openEarable: widget.openEarable,
-          sensorName: 'PPG',
-          chartTitle: 'PPG',
-          shortTitle: 'PPG',
-        ),
-        Earable3DModel(widget.openEarable),
-      ],
+      body: TabBarView(
+        controller: _tabController,
+        children: dataChartWidgets,
+      ),
     );
   }
 }
