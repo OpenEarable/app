94e34a7e888e7971df2b8d9575221df5a6d3fb9a
Jonas Greifenhain <cadivus@daverkomp.de>
2024-10-23 22:09:30 +0200

    Add option to pass precalculated row and column count
    E.g. for decisions whether to show the grid

--- open_earable/lib/shared/square_children_grid.dart
+++ open_earable/lib/shared/square_children_grid.dart
@@ -3,50 +3,78 @@ import 'package:flutter/material.dart';
 /// Creates a grid that tries to keep the children as square as possible
 class SquareChildrenGrid extends StatelessWidget {
   final List<Widget> children;
+  final int? precalculatedRows;
+  final int? precalculatedColumns;
 
   const SquareChildrenGrid({
     super.key,
     required this.children,
+    this.precalculatedRows,
+    this.precalculatedColumns,
   });
+
+  static (int, int) calculateColumnsAndRows(
+    double width,
+    double height,
+    int childrenCount,
+  ) {
+    double aspectRatio = width / height;
+
+    // Define a number of columns and rows to get an aspect ration close to
+    // the available space
+    double childrenAspectRation = double.maxFinite;
+    int rows = 1;
+    int cols = childrenCount;
+    for (int newRows = 1; newRows <= childrenCount; ++newRows) {
+      int newCols = (childrenCount / newRows).ceil();
+      double newChildrenAspectRation = newCols / newRows;
+
+      // Check if the difference between widget size aspect ration
+      // and rows to columns ratio decreased
+      if ((newChildrenAspectRation - aspectRatio).abs() <
+          (childrenAspectRation - aspectRatio).abs()) {
+        childrenAspectRation = newChildrenAspectRation;
+        cols = newCols;
+        rows = newRows;
+      } else {
+        break;
+      }
+    }
+
+    // Remove empty rows
+    bool changes = true;
+    while (changes) {
+      int emptyCells = rows * cols - childrenCount;
+      if (emptyCells >= cols && rows > 1) {
+        --rows;
+        emptyCells -= cols;
+        continue;
+      }
+      changes = false;
+    }
+    return (cols, rows);
+  }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
         int childrenCount = children.length;
-        double aspectRatio = constraints.maxWidth / constraints.maxHeight;
+        late int rows;
+        late int cols;
 
-        // Define a number of columns and rows to get an aspect ration close to
-        // the available space
-        double childrenAspectRation = double.maxFinite;
-        int rows = 1;
-        int cols = childrenCount;
-        for (int newRows = 1; newRows <= childrenCount; ++newRows) {
-          int newCols = (childrenCount / newRows).ceil();
-          double newChildrenAspectRation = newCols / newRows;
+        if (precalculatedRows != null && precalculatedColumns != null) {
+          rows = precalculatedRows!;
+          cols = precalculatedColumns!;
+        } else {
+          (int, int) rowsAndCols = calculateColumnsAndRows(
+            constraints.maxWidth,
+            constraints.maxHeight,
+            childrenCount,
+          );
 
-          // Check if the difference between widget size aspect ration
-          // and rows to columns ratio decreased
-          if ((newChildrenAspectRation - aspectRatio).abs() <
-              (childrenAspectRation - aspectRatio).abs()) {
-            childrenAspectRation = newChildrenAspectRation;
-            cols = newCols;
-            rows = newRows;
-          } else {
-            break;
-          }
-        }
-
-        // Remove empty rows
-        bool changes = true;
-        while (changes) {
-          int emptyCells = rows * cols - childrenCount;
-          if (emptyCells >= cols && rows > 1) {
-            --rows;
-            emptyCells -= cols;
-            continue;
-          }
-          changes = false;
+          cols = rowsAndCols.$1;
+          rows = rowsAndCols.$2;
         }
 
         return Column(
