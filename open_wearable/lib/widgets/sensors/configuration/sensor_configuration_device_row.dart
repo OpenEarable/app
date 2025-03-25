54c1527f34dd5f3524ada77372e7259874189217
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-03-25 12:01:57 +0100

    open_wearable/lib/widgets/configuration/sensor_configuration_device_row.dart: fixed alignment

--- open_wearable/lib/widgets/sensors/configuration/sensor_configuration_device_row.dart
+++ open_wearable/lib/widgets/sensors/configuration/sensor_configuration_device_row.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:open_earable_flutter/open_earable_flutter.dart';
 import 'package:open_wearable/widgets/sensors/configuration/sensor_configuration_value_row.dart';
 
@@ -11,46 +12,40 @@ class SensorConfigurationDeviceRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            // Device Title
-            Row(
-              children: [
-                Text(
-                  device.name,
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
-                ),
-                Spacer(),
-                if (device is DeviceIdentifier)
-                  FutureBuilder(
-                    future: (device as DeviceIdentifier).readDeviceIdentifier(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done) {
-                        return Text(snapshot.data.toString());
-                      } else {
-                        return CircularProgressIndicator();
-                      }
-                    },
-                  )
-              ],
+      child: Column(
+        children: [
+          PlatformListTile(
+            title: Text(
+              device.name,
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
             ),
-            if (device is SensorConfigurationManager)
-              ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: (device as SensorConfigurationManager).sensorConfigurations.length,
-                itemBuilder: (context, index) {
-                  return SensorConfigurationValueRow(
-                    sensorConfiguration: (device as SensorConfigurationManager).sensorConfigurations[index]
-                  );
+            trailing: (device is DeviceIdentifier) ?
+              FutureBuilder(
+                future: (device as DeviceIdentifier).readDeviceIdentifier(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return Text(snapshot.data.toString());
+                  } else {
+                    return CircularProgressIndicator();
+                  }
                 },
               )
-            else
-              Text("This device does not support sensors"),
-          ],
-        ),
+              : null,
+          ),
+          if (device is SensorConfigurationManager)
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: (device as SensorConfigurationManager).sensorConfigurations.length,
+              itemBuilder: (context, index) {
+                return SensorConfigurationValueRow(
+                  sensorConfiguration: (device as SensorConfigurationManager).sensorConfigurations[index]
+                );
+              },
+            )
+          else
+            Text("This device does not support sensors"),
+        ],
       ),
     );
   }
