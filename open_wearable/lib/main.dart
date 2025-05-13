dac23a00134ec3bc6173d945002cfbbdbc1cfeb8
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-05-13 10:43:46 +0200

    removed unused notifier

--- open_wearable/lib/main.dart
+++ open_wearable/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:logger/logger.dart';
-import 'package:open_wearable/view_models/sensor_configuration_provider.dart';
 import 'package:open_wearable/widgets/home_page.dart';
 import 'package:provider/provider.dart';
 import 'package:open_earable_flutter/open_earable_flutter.dart' as oe;
@@ -27,7 +26,6 @@ void main() {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => WearablesProvider()),
-        ChangeNotifierProvider(create: (context) => SensorConfigurationProvider(),)
       ],
       child: const MyApp()
     )
