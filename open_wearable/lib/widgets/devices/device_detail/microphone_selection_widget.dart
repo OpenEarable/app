d333dddcff34a0d280cb9a75f54eba122b88ff0c
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-05-20 13:49:20 +0200

    refactored to avoid rendering of not changing views

---
+++ open_wearable/lib/widgets/devices/device_detail/microphone_selection_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:open_earable_flutter/open_earable_flutter.dart';
+
+class MicrophoneSelectionWidget extends StatefulWidget {
+  final MicrophoneManager device;
+
+  const MicrophoneSelectionWidget({
+    super.key,
+    required this.device,
+  });
+
+  @override
+  State<MicrophoneSelectionWidget> createState() => _MicrophoneSelectionWidgetState();
+}
+
+class _MicrophoneSelectionWidgetState extends State<MicrophoneSelectionWidget> {
+  Microphone? _selectedMicrophone;
+
+  @override
+  void initState() {
+    super.initState();
+    _getSelectedMicrophone();
+  }
+
+  Future<void> _getSelectedMicrophone() async {
+    final mode = await widget.device.getMicrophone();
+    setState(() {
+      _selectedMicrophone = mode;
+    });
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformWidget(
+      cupertino:(context, platform) => CupertinoSlidingSegmentedControl(
+        children: {
+          for (var item in widget.device.availableMicrophones)
+            item : Text(item.key)
+        },
+        onValueChanged: (Microphone? mode) {
+          if (mode == null) return;
+          widget.device.setMicrophone(mode);
+        },
+        groupValue: _selectedMicrophone,
+      ),
+      material: (context, platform) => SegmentedButton<Microphone>(
+        segments: 
+          widget.device.availableMicrophones.map((item) {
+            return ButtonSegment<Microphone>(
+              value: item,
+              label: Text(item.key),
+            );
+          }).toList(),
+          onSelectionChanged: (Set<Microphone> selected) {
+            if (selected.isEmpty) return;
+            widget.device.setMicrophone(selected.first);
+            setState(() {
+              _selectedMicrophone = selected.first;
+            });
+          },
+          selected: _selectedMicrophone != null ? { _selectedMicrophone! } : {},
+          emptySelectionAllowed: true,
+        ),
+    );
+  }
+}
