a0dc101e1fa9d80483345dfd35e22b7bb01369db
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-05-21 10:08:18 +0200

    open_wearable/lib/widgets/sensors/values/sensor_chart.dart: use FL Charts instead of community charts flutter

--- open_wearable/lib/widgets/sensors/values/sensor_chart.dart
+++ open_wearable/lib/widgets/sensors/values/sensor_chart.dart
@@ -1,106 +1,87 @@
 import 'dart:async';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:open_earable_flutter/open_earable_flutter.dart';
-import 'package:community_charts_flutter/community_charts_flutter.dart' as charts;
 
 class SensorChart extends StatefulWidget {
   final Sensor sensor;
   final bool allowToggleAxes;
-  /// Time window in seconds for which data is displayed
-  final int timeWindow;
+  final int timeWindow; // in seconds
 
-  const SensorChart({super.key, required this.sensor, this.allowToggleAxes = true, this.timeWindow = 5});
+  const SensorChart({
+    super.key,
+    required this.sensor,
+    this.allowToggleAxes = true,
+    this.timeWindow = 5,
+  });
 
   @override
   State<SensorChart> createState() => _SensorChartState();
 }
 
 class _SensorChartState extends State<SensorChart> {
-  List<charts.Series<ChartData, int>> _chartData = [];
-  final List<ChartData> _dataPoints = [];
-
-  // Track which axes are enabled
   late Map<String, bool> _axisEnabled;
-
+  late Map<String, List<FlSpot>> _axisData;
   StreamSubscription<SensorValue>? _sensorStreamSubscription;
+  late int _startTime;
 
   @override
   void initState() {
     super.initState();
-    _axisEnabled = {for (var name in widget.sensor.axisNames) name: true};
+    _axisEnabled = {
+      for (var name in widget.sensor.axisNames) name: true,
+    };
+    _axisData = {
+      for (var name in widget.sensor.axisNames) name: [],
+    };
+    _startTime = DateTime.now().millisecondsSinceEpoch;
     _listenToSensorStream();
-  }
-
-  @override
-  void didUpdateWidget(SensorChart oldWidget) {
-    _listenToSensorStream();
-    super.didUpdateWidget(oldWidget);
   }
 
   void _listenToSensorStream() {
+    _sensorStreamSubscription?.cancel();
     _sensorStreamSubscription = widget.sensor.sensorStream.listen((sensorValue) {
+      final currentTime = DateTime.now().millisecondsSinceEpoch;
+      final elapsedTime = (currentTime - _startTime) / 1000.0; // in seconds
+
       setState(() {
-        // Add new data points
         for (int i = 0; i < widget.sensor.axisCount; i++) {
-          if (sensorValue is SensorDoubleValue) {
-            _dataPoints.add(ChartData(sensorValue.timestamp, sensorValue.values[i], widget.sensor.axisNames[i]));
-          } else if (sensorValue is SensorIntValue) {
-            _dataPoints.add(ChartData(sensorValue.timestamp, sensorValue.values[i].toDouble(), widget.sensor.axisNames[i]));
-          }
+          final axisName = widget.sensor.axisNames[i];
+          final value = sensorValue is SensorDoubleValue
+              ? sensorValue.values[i]
+              : (sensorValue as SensorIntValue).values[i].toDouble();
+
+          _axisData[axisName]?.add(FlSpot(elapsedTime, value));
+
+          // Remove data older than timeWindow
+          _axisData[axisName] = _axisData[axisName]!
+              .where((spot) => elapsedTime - spot.x <= widget.timeWindow)
+              .toList();
         }
-
-        // Remove data older than 5 seconds
-        int cutoffTime = sensorValue.timestamp - (widget.timeWindow * pow(10, -widget.sensor.timestampExponent) as int);
-        _dataPoints.removeWhere((data) => data.time < cutoffTime);
-
-        _updateChartData();
       });
     });
-  }
-
-  void _updateChartData() {
-    // Update chart data based on enabled axes
-    _chartData = [
-      for (int i = 0; i < widget.sensor.axisCount; i++)
-        if (_axisEnabled[widget.sensor.axisNames[i]] ?? false)
-          charts.Series<ChartData, int>(
-            id: widget.sensor.axisNames[i],
-            colorFn: (_, __) => charts.MaterialPalette.blue.makeShades(widget.sensor.axisCount)[i],
-            domainFn: (ChartData point, _) => point.time,
-            measureFn: (ChartData point, _) => point.value,
-            data: _dataPoints.where((point) => point.axisName == widget.sensor.axisNames[i]).toList(),
-          ),
-    ];
   }
 
   void _toggleAxis(String axisName, bool value) {
     setState(() {
       _axisEnabled[axisName] = value;
-      _updateChartData();
     });
   }
 
   @override
+  void dispose() {
+    _sensorStreamSubscription?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // Filter only enabled axes data for scaling
-    final filteredPoints = _dataPoints
-        .where((point) => _axisEnabled[point.axisName] ?? false)
+    final enabledAxes = widget.sensor.axisNames
+        .where((axis) => _axisEnabled[axis] ?? false)
         .toList();
-
-    final xValues = filteredPoints.map((e) => e.time).toList();
-    final yValues = filteredPoints.map((e) => e.value).toList();
-
-    final int? xMin = xValues.isNotEmpty ? xValues.reduce((a, b) => a < b ? a : b) : null;
-    final int? xMax = xValues.isNotEmpty ? xValues.reduce((a, b) => a > b ? a : b) : null;
-
-    final double? yMin = yValues.isNotEmpty ? yValues.reduce((a, b) => a < b ? a : b) : null;
-    final double? yMax = yValues.isNotEmpty ? yValues.reduce((a, b) => a > b ? a : b) : null;
 
     return Column(
       children: [
-        // Checkbox controls for each axis
         if (widget.allowToggleAxes)
           Wrap(
             spacing: 8.0,
@@ -110,74 +91,35 @@ class _SensorChartState extends State<SensorChart> {
                 children: [
                   Checkbox(
                     value: _axisEnabled[axisName],
-                    onChanged: (value) => _toggleAxis(axisName, value ?? false),
+                    onChanged: (value) =>
+                        _toggleAxis(axisName, value ?? false),
                   ),
                   Text(axisName),
                 ],
               );
             }).toList(),
           ),
-        // Chart display
         Expanded(
-          child: _chartData.isEmpty ?
-            Center(
-              child: Text('No data available'),
-            ) :
-            charts.LineChart(
-              _chartData,
-              animate: false,
-              domainAxis: charts.NumericAxisSpec(
-                viewport: xMin != null && xMax != null
-                    ? charts.NumericExtents(xMin.toDouble(), xMax.toDouble())
-                    : null,
-                tickProviderSpec: const charts.BasicNumericTickProviderSpec(zeroBound: false, desiredMinTickCount: 3),
-              ),
-              primaryMeasureAxis: charts.NumericAxisSpec(
-                viewport: yMin != null && yMax != null
-                    ? charts.NumericExtents(yMin, yMax)
-                    : null,
-                tickProviderSpec: const charts.BasicNumericTickProviderSpec(zeroBound: false, desiredMinTickCount: 3),
-              ),
-              behaviors: [
-                charts.SeriesLegend(),
-                charts.ChartTitle(
-                  'Time (${_timestampUnitPrefix(widget.sensor.timestampExponent)}s)',
-                  behaviorPosition: charts.BehaviorPosition.bottom
-                ),
-                charts.ChartTitle(widget.sensor.axisUnits.first, behaviorPosition: charts.BehaviorPosition.start),
-              ],
+          child: LineChart(
+            LineChartData(
+              lineTouchData: LineTouchData(enabled: false),
+              gridData: FlGridData(show: true),
+              titlesData: FlTitlesData(show: true),
+              borderData: FlBorderData(show: false),
+              lineBarsData: enabledAxes.map((axisName) {
+                return LineChartBarData(
+                  spots: _axisData[axisName] ?? [],
+                  isCurved: false,
+                  barWidth: 2,
+                  isStrokeCapRound: true,
+                  dotData: FlDotData(show: false),
+                );
+              }).toList(),
             ),
+            duration: const Duration(microseconds: 0),
+          ),
         ),
       ],
     );
   }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _sensorStreamSubscription?.cancel();
-  }
-}
-
-String _timestampUnitPrefix(int exponent) {
-  switch (exponent) {
-    case 0:
-      return '';
-    case -3:
-      return 'm';
-    case -6:
-      return 'µ';
-    case -9:
-      return 'n';
-    default:
-      return '?';
-  }
-}
-
-class ChartData {
-  final int time; // Timestamp in milliseconds
-  final double value; // Sensor value
-  final String axisName; // Name of the axis
-
-  ChartData(this.time, this.value, this.axisName);
 }
