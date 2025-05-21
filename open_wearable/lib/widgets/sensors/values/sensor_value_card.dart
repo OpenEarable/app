931038fc71e858e8f71a3f0da3e91677be578863
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-05-21 15:20:09 +0200

    improved charts performance

--- open_wearable/lib/widgets/sensors/values/sensor_value_card.dart
+++ open_wearable/lib/widgets/sensors/values/sensor_value_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:open_earable_flutter/open_earable_flutter.dart';
 import 'package:open_wearable/view_models/sensor_data_provider.dart';
 import 'package:open_wearable/widgets/sensors/values/sensor_chart.dart';
