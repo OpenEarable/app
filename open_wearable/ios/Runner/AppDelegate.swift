c2eae5d16f2312e30c4e3b787edc1d7bb2ddb3bf
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-07-11 15:45:35 +0200

    open_wearable/ios/Runner/AppDelegate.swift: added method to open files app

--- open_wearable/ios/Runner/AppDelegate.swift
+++ open_wearable/ios/Runner/AppDelegate.swift
@@ -6,7 +6,27 @@ import UIKit
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+  ) -> Bool {    
+    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "edu.teco.open_folder", binaryMessenger: controller.binaryMessenger)
+    
+    channel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+      if call.method == "openFolder", let args = call.arguments as? [String: Any], let path = args["path"] as? String {
+        guard let url = URL(string: path) else {
+          result(FlutterError(code: "INVALID_ARGUMENT", message: "Invalid folder path", details: nil))
+          return
+        }
+        if UIApplication.shared.canOpenURL(url) {
+          UIApplication.shared.open(url, options: [:], completionHandler: nil)
+          result(nil)
+        } else {
+          result(FlutterError(code: "UNAVAILABLE", message: "Cannot open folder \(path)", details: nil))
+        }
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
