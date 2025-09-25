2c780484ef06aa958dc3ce7d41bd1f2371261b52
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2025-09-25 12:05:41 +0200

    open_wearable/lib/widgets/devices/devices_page.dart: removed unused import

--- open_wearable/lib/widgets/devices/devices_page.dart
+++ open_wearable/lib/widgets/devices/devices_page.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
