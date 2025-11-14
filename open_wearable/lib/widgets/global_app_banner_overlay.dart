9809d1489a15137580f03c1b1e8c58f160092887
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2025-11-14 16:10:45 +0100

    open_wearable/lib/widgets/global_app_banner_overlay.dart: use key of banner for padding around banner

--- open_wearable/lib/widgets/global_app_banner_overlay.dart
+++ open_wearable/lib/widgets/global_app_banner_overlay.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:open_wearable/view_models/app_banner_controller.dart';
 import 'package:provider/provider.dart';
+
+import '../view_models/app_banner_controller.dart';
 
 class GlobalAppBannerOverlay extends StatelessWidget {
   final Widget child;
@@ -24,10 +25,9 @@ class GlobalAppBannerOverlay extends StatelessWidget {
         return Directionality(
           textDirection: textDirection,
           child: Stack(
-            alignment: Alignment.topLeft, // non-directional
+            alignment: Alignment.topLeft,
             children: [
               child,
-
               if (hasBanners)
                 Positioned(
                   top: MediaQuery.of(context).padding.top,
@@ -37,18 +37,17 @@ class GlobalAppBannerOverlay extends StatelessWidget {
                     bottom: false,
                     child: SingleChildScrollView(
                       scrollDirection: Axis.horizontal,
-                      // rubber-band even when only one banner
                       physics: const BouncingScrollPhysics(
                         parent: AlwaysScrollableScrollPhysics(),
                       ),
-
                       padding: const EdgeInsets.symmetric(vertical: 4),
                       child: Row(
                         children: [
                           for (final banner in banners)
                             Padding(
                               padding:
-                                  const EdgeInsets.symmetric(horizontal: 8),
+                                const EdgeInsets.symmetric(horizontal: 8),
+                              key: banner.key,
                               child: banner,
                             ),
                         ],
