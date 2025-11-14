bc85527dee5309176f3070ad5a6d381c227831f5
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2025-11-14 14:09:29 +0100

    open_wearable/lib/widgets/app_banner.dart: added close button to each banner

--- open_wearable/lib/widgets/app_banner.dart
+++ open_wearable/lib/widgets/app_banner.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../view_models/app_banner_controller.dart';
 
 class AppBanner extends StatelessWidget {
   final Widget content;
@@ -12,13 +15,40 @@ class AppBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      color: backgroundColor,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: content,
-      ),
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Card(
+          elevation: 3,
+          color: backgroundColor,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 40, 16),
+            child: content,
+          ),
+        ),
+
+        Positioned(
+          top: 8,
+          right: 8,
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            constraints: const BoxConstraints(
+              minWidth: 32,
+              minHeight: 32,
+            ),
+            style: ButtonStyle(
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            splashRadius: 18,
+            icon: const Icon(Icons.close, size: 20, color: Colors.white),
+            onPressed: () {
+              final controller =
+                  Provider.of<AppBannerController>(context, listen: false);
+              controller.hideBanner(this);
+            },
+          ),
+        ),
+      ],
     );
   }
 }
