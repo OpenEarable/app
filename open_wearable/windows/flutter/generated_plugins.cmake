70fc9e4e7998c4135cba4e90027ec5e0e93061b3
Oliver Bagge <bagge@teco.edu>
2026-01-16 17:10:03 +0100

    add custom audio waveform to the Sensors tab with device selector

--- open_wearable/windows/flutter/generated_plugins.cmake
+++ open_wearable/windows/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
   file_selector_windows
   permission_handler_windows
+  record_windows
   share_plus
   universal_ble
   url_launcher_windows
