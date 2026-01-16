8f1805d5c7b4ea57e029c7ea2c1e0130b1837703
Oliver Bagge <bagge@teco.edu>
2026-01-16 17:10:03 +0100

    add custom audio waveform to the Sensors tab with device selector

--- open_wearable/linux/flutter/generated_plugins.cmake
+++ open_wearable/linux/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_linux
   file_selector_linux
   open_file_linux
+  record_linux
   url_launcher_linux
 )
 
--- open_wearable/macos/Flutter/GeneratedPluginRegistrant.swift
+++ open_wearable/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import flutter_archive
 import mcumgr_flutter
 import open_file_mac
 import package_info_plus
+import record_macos
 import share_plus
 import shared_preferences_foundation
 import universal_ble
@@ -28,6 +29,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   McumgrFlutterPlugin.register(with: registry.registrar(forPlugin: "McumgrFlutterPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UniversalBlePlugin.register(with: registry.registrar(forPlugin: "UniversalBlePlugin"))
