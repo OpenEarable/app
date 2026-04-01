4d9c0dd612ba5fbbe6e4f9c2a0d67fa4d279ad25
Oliver Bagge <bagge@teco.edu>
2026-04-01 16:06:23 +0300

    Fixed an iOS BLE state issue that previously required restarting the iPhone to reconnect to OpenEarable, by deduplicating disconnect handling and stabilizing reconnect/scan behavior.

--- open_wearable/macos/Flutter/GeneratedPluginRegistrant.swift
+++ open_wearable/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import file_selector_macos
 import flutter_archive
 import open_file_mac
 import package_info_plus
-import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import universal_ble
@@ -25,7 +24,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterArchivePlugin.register(with: registry.registrar(forPlugin: "FlutterArchivePlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UniversalBlePlugin.register(with: registry.registrar(forPlugin: "UniversalBlePlugin"))
