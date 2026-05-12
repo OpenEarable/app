3f5c38db0386faed9f0e5ff2a036f90182faab73
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2026-05-12 11:47:35 +0200

    chore(pubspec): changed dependency of mcumgr_flutter and open_earable_flutter to development versions

--- open_wearable/macos/Flutter/GeneratedPluginRegistrant.swift
+++ open_wearable/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import device_info_plus
 import file_picker
 import file_selector_macos
 import flutter_archive
+import mcumgr_flutter
 import open_file_mac
 import package_info_plus
 import share_plus
@@ -24,6 +25,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterArchivePlugin.register(with: registry.registrar(forPlugin: "FlutterArchivePlugin"))
+  McumgrFlutterPlugin.register(with: registry.registrar(forPlugin: "McumgrFlutterPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
