53d57dbc3c3855d40511d778fb8aef6ecae07f7b
Oliver Bagge <bagge@teco.edu>
2026-05-13 21:38:44 +0300

    fix recorder in macos

--- open_wearable/windows/flutter/generated_plugins.cmake
+++ open_wearable/windows/flutter/generated_plugins.cmake
@@ -6,12 +6,14 @@ list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
   file_selector_windows
   permission_handler_windows
+  record_windows
   share_plus
   universal_ble
   url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
